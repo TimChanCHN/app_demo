602287c223e64203f51f25d938d2a47afb8a1c69
TimChanCHN <874797636@qq.com>
2022-04-14 21:46:26 +0800

    update

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@
 int main(void)
 {
   uint8_t flag = 0;
-  uint32_t count = g_systick_obj.systick_ops.systick_get(&g_systick_obj.systick_cfg);
+  uint32_t count = g_systick_obj.systick_ops.systick_get();
 
   g_systick_obj.systick_ops.systick_init(&g_systick_obj.systick_cfg);
 
@@ -19,8 +19,8 @@ int main(void)
   g_led_obj.gpio_ops.gpio_output_set(&g_led_obj.gpio_cfg, 0);
 
   g_systick_obj.systick_ops.delay_us(&g_systick_obj.systick_cfg, 1000);
-  trace_info("loop\r\n");
-  trace_debug("debug\r\n");
+  // trace_info("loop\r\n");
+  // trace_debug("debug\r\n");
   while (1)
   {
     #if 1
@@ -29,9 +29,9 @@ int main(void)
     g_led_obj.gpio_ops.gpio_output_set(&g_led_obj.gpio_cfg, 0);
     g_systick_obj.systick_ops.delay_ms(&g_systick_obj.systick_cfg, 1000);
     #else
-    if (g_systick_obj.systick_ops.systick_get(&g_systick_obj.systick_cfg) - count > 1000)
+    if (g_systick_obj.systick_ops.systick_get() - count > 1000)
     {
-      count = g_systick_obj.systick_ops.systick_get(&g_systick_obj.systick_cfg);
+      count = g_systick_obj.systick_ops.systick_get();
       flag = !flag;
     }
     g_led_obj.gpio_ops.gpio_output_set(&g_led_obj.gpio_cfg, flag);
