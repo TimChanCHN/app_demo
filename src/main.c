b37030162326466910084dac900be483e1e688e2
TimChanCHN <874797636@qq.com>
2022-08-04 22:14:44 +0800

    lvgl demo

--- src/main.c
+++ src/main.c
@@ -62,11 +62,6 @@ int main(void)
 
 	TIMER_CREATE(&m_test_timer, false, false, test_timer_handler);
 	TIMER_START(m_test_timer, 500);
-	
-	// g_tftlcd9341_obj.tftlcd_ops.init(&g_tftlcd9341_obj.tftlcd_cfg, &g_tftlcd9341_obj.tftlcd_ops);
-	// g_tftlcd3510_obj.tftlcd_ops.init(&g_tftlcd3510_obj.tftlcd_cfg, &g_tftlcd3510_obj.tftlcd_ops);
-	// g_touch_obj.touch_ops.init(&g_touch_obj.touch_cfg);
-	// trace_info("touch init ok\r\n");
 
 	// lvgl设置
 	lv_init();
@@ -74,140 +69,40 @@ int main(void)
 	lv_port_indev_init();
 	lv_port_fs_init();
 
-	#if 0
-	// test sdio
-	#if 0
-    while (g_sdio_obj.sdio_ops.sd_init(&g_sdio_obj.sdio_cfg))
-	{
-		g_systick_obj.systick_ops.delay_ms(1000);
-		trace_info("sd init fail...\r\n");
-	}
-	trace_info("sd init ok...\r\n");
-	#endif
 
-	uint8_t *buf;
-	uint16_t cnt = 0;
-	buf = (uint8_t *)malloc(512);
-	trace_info("buf addr = 0x%x\r\n", buf);
-
-	g_sdio_obj.sdio_ops.sd_read_disk(&g_sdio_obj.sdio_cfg, buf, 0, 1);
-
-	trace_info("sector 0 data: \r\n");
-	for (cnt = 0; cnt < 512; cnt++)
-	{
-		printf("%x ", buf[cnt]);
-	}
-	printf("\r\n");
-	free(buf);
-
-	// test fatfs
-	FATFS fs;
-	FRESULT fres;
-
-	// fres = f_mount(&fs, "0:", 1);
-	// while (fres)
-	// {
-	// 	g_systick_obj.systick_ops.delay_ms(1000);
-	// 	trace_info("mount sd error %d\r\n", fres);
-	// }
-	// trace_info("mount ok\r\n");
-
-	FIL *file;
-	const char *path = "0:/lvgl.txt";
-
-	file = malloc(sizeof(FIL));
-
-	fres = f_open(file, path, (FA_READ | FA_WRITE));
-	if (fres)
-	{
-		trace_info("open file fail: %d\r\n", fres);
-		return ;
-	}
-	else
-	{
-		trace_info("open file ok\r\n");
-	}
-
-	char buff[] = "cxtcfn";
-	uint16_t bw;
-    fres = f_write(file, buff, sizeof(buff), &bw);
-    if (fres != FR_OK)
-    {
-        trace_info("write file error : %d\r\n", fres);
-    }
-    else
-    {
-        trace_info("write ok. write %d bytes.\r\n", bw);
-    }
-
-	char buff1[10];
-	uint16_t br;
-	fres = f_lseek(file, 0);
-    fres = f_read(file, buff1, sizeof(buff), &br);
-    if (fres != FR_OK)
-    {
-        trace_info("read file error : %d\r\n", fres);
-    }
-    else
-    {
-        trace_info("read ok. read %d bytes.\r\n", bw);
-		trace_info("buff : %s.\r\n", buff1);
-    }
-
-	fres = f_close(file);
-	if (fres)
-	{
-		trace_info("close file fail: %d\r\n", fres);
-		return ;
-	}
-	else
-	{
-		trace_info("close file ok\r\n");
-	}
-
-	free(sizeof(FIL));
-	#endif
-
-	#if 1
 	lv_fs_res_t fs_res;
 	lv_fs_file_t *file1 = lv_mem_alloc(sizeof(lv_fs_file_t));
-	const char *path1 = "0:/lvgl.txt";
+	const char *path1 = "S:/test/lvgl.txt";
 
 	fs_res=lv_fs_open(file1, path1, LV_FS_MODE_WR | LV_FS_MODE_RD);
 	if(fs_res != LV_FS_RES_OK)
 		printf("open error! code:%d\r\n",fs_res);
 
 	#if 1
-	uint16_t byte = 0;
-	uint8_t read_buff[20] = {0};
-	char buff3[] = "cxtcfn";
+	uint16_t bw, br = 0;
+	uint8_t read_buff[30] = {0};
+	char buff3[] = "TimChan don't like valentine";
 
-	fs_res = lv_fs_seek(file1, 0);
-	// fs_res = f_write((FIL*)file1->file_d, buff3, sizeof(buff3), &byte);
-	fs_res=lv_fs_write(file1, buff3, sizeof(buff3)-1, &byte);
+	fs_res=lv_fs_write(file1, (void*)buff3, sizeof(buff3), &bw);
 	if(fs_res != LV_FS_RES_OK)
 	{
 		printf("write error! code:%d\r\n",fs_res);
 	}
 	else
 	{
-		trace_info("write ok. write %d bytes.\r\n", byte);
+		trace_info("write ok. write %d bytes.\r\n", bw);
 	}
 
 	fs_res = lv_fs_seek(file1, 0);
-	// fs_res = f_lseek((FIL*)file1->file_d, 0);
-	fs_res = lv_fs_read(file1,read_buff,sizeof(buff3)-1,&byte);
-	trace_info("start read\r\n");
-	// fs_res = lv_fs_read(file1, read_buff, 6, &byte);
-	// fs_res = f_read((FIL*)file1->file_d, read_buff, sizeof(buff3), &byte);
+	fs_res = lv_fs_read(file1,read_buff,sizeof(buff3),&br);
 	if(fs_res != LV_FS_RES_OK)
 	{
 		printf("read error! code:%d\r\n",fs_res);
 	}
 	else
 	{
-	trace_info("read ok. %d bytes to be read.\r\n",byte);
-	trace_info("read buf = %s\r\n", read_buff);
+		trace_info("read ok. %d bytes to be read.\r\n",br);
+		trace_info("read buf = %s\r\n", read_buff);
 	}
 	#endif
 
@@ -217,8 +112,6 @@ int main(void)
 
 	trace_info("xxx\r\n");
 	lv_mem_free(file1);
-	#endif
-
 
 	trace_info("loop\r\n");
 	trace_debug("debug\r\n");
